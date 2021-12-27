15dc98a17e38fd7cc0723bdce0f7c74c7b04589c
Ubuntu <ubuntu@ip-172-31-9-95.us-east-2.compute.internal>
2021-12-27 09:00:59 +0000

    20211227_finished

--- controller/calendars.js
+++ controller/calendars.js
@@ -126,7 +126,6 @@ const CalendarsController = {
         }
       })
     } catch (err) {
-      console.log("delete error: ", err)
       res.status(400)
       res.json({
         success: false,
@@ -188,11 +187,11 @@ const CalendarsController = {
           }
         })
         const startPrefix =
-          Number(moment(startTime).tz("Asia/Taipei").format("h")) >= 12
+          Number(moment(startTime).tz("Asia/Taipei").format("H")) >= 12
             ? "下午"
             : "上午"
         const endPrefix =
-          Number(moment(finishTime).tz("Asia/Taipei").format("h")) >= 12
+          Number(moment(finishTime).tz("Asia/Taipei").format("H")) >= 12
             ? "下午"
             : "上午"
         return {
